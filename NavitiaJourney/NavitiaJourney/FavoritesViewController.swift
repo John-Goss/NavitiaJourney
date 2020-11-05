b2698987db6451d1a9c0c6faa1d76b5bd546ca84
John Goss <john.redirecting@gmail.com>
2020-11-05 18:00:53 +0100

    Configure AppDelegate & SceneDelegate for iOS11+ compatibility

---
+++ NavitiaJourney/NavitiaJourney/FavoritesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavitiaJourney
+//
+//  Created by John on 05/11/2020.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
